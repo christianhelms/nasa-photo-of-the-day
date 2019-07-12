933abf4f2ce6ae0cf97ee53634c2d049294d0f24
christianhelms <fonticons-dev@Fonticonss-MacBook-Pro.local>
2019-07-12 10:47:20 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Title from "./Components/Title"
-import axios from "axios"
+import Title from "./Components/Title";
+import axios from "axios";
+import styled from "styled-components";
 
 function App() {
   const [nasa, setNasa] = useState([]);
 
   const fetchPics = () => {
     axios
-    .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
       .then(nasa => {
         console.log(nasa);
         setNasa(nasa.data);
       })
       .catch(err => console.log("OUCH :("));
-  }
+  };
 
   useEffect(() => {
     fetchPics();
   }, []);
 
   return (
-    <div className="App">
-    <Title />
-    <div className="Pic">
-      <img src={nasa.hdurl} width={"480px"}/>
-    </div>
-    </div>
+    <Container className="App">
+      <Title />
+      <div className="Pic">
+        <img src={nasa.hdurl} width={"480px"} />
+      </div>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  text-align: center;
+`;
+
 export default App;
+
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import styled from "styled-components";
 
 export default function Title() {
     var event = new Date();
     
     return (
         <div>
-            <h1>Here's Your Pic of the Day for { event.toLocaleString('en', { weekday: 'long', month: 'long', day: '2-digit', year: 'numeric' }) }</h1>
+            <Header>Here's Your Pic of the Day for { event.toLocaleString('en', { weekday: 'long', month: 'long', day: '2-digit', year: 'numeric' }) }</Header>
         </div>
     )
 }
+
+const Header = styled.h1`
+  size: 50px;
+  color: red;
+`;
